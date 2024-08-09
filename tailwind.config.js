a84fc40ed5b1eea9599d6cf857f84b474edd8151
Sahan Kaushalya <mkskaushalya@gmail.com>
2024-08-10 03:30:07 +0530

    basic setup

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    content: {
+        // Specify files for PurgeCSS to search through
+        
+    },
+    theme: {
+        extend: {
+            colors: {
+                'primary': '#FF6363',
+                'secondary': {
+                    100: '#E2E2D5',
+                    200: '#888883',
+                },
+            },
+            fontFamily: {
+                body: ['Roboto', 'sans-serif'],
+            }
+        }
+    },
+}; 
