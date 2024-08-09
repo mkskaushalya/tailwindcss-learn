4f84544fb74e3c354b2b508cb9d98678b9a04744
Sahan Kaushalya <mkskaushalya@gmail.com>
2024-08-10 04:44:07 +0530

    updatwe

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: {
-        // Specify files for PurgeCSS to search through
-        
+  content: [],
+  theme: {
+    extend: {
+      
     },
-    theme: {
-        extend: {
-            colors: {
-                'primary': '#FF6363',
-                'secondary': {
-                    100: '#E2E2D5',
-                    200: '#888883',
-                },
-            },
-            fontFamily: {
-                body: ['Roboto', 'sans-serif'],
-            }
-        }
-    },
-}; 
+  },
+  plugins: [],
+}
+
